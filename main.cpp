9037186e6f6eafebcb513128fb05c3bba74f16e0
Alan González <89163839+XxGonanxX@users.noreply.github.com>
2022-11-29 10:11:12 -0600

    Código finalizado.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,79 @@
 // =================================================================
 //
 // File: main.cpp
-// Author:
-// Date:
+// Author: Alan Patricio González Bernal
+//		   Ian Joab Padrón Corona 
+//		   Diego Perdomo Salcedo
+// Date: 30/11/2022
 //
 // =================================================================
 #include <iostream>
+#include <map>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	return 0;
+
+	// main, <inputfile.txt> outputfile.txt
+  ifstream inputfile;
+  ofstream outputfile;
+
+  if(argc != 3){
+    cout<< "Invocación incorrecta!" << endl; 
+  }
+  
+  inputfile.open(argv[1]); // entrada a array
+  if(inputfile.fail()){
+
+    cout << "No se pudo leer el archivo de entrada bro" << endl;
+    return -1;
+  
+  }
+
+  outputfile.open(argv[2]);
+  if(inputfile.fail()){
+
+    cout << "No se pudo abrir el archivo de salida bro" << endl;
+    return -1;
+  
+  }
+	int a, b;
+	inputfile >> a >> b;
+
+	map<string, int> UnlimitedSeas;
+	for(int i = 0; i < a; i++){
+
+		int c;
+		string d;
+		inputfile >> d >> c;
+		UnlimitedSeas[d] = c;
+
+	}
+
+	for(int i = 0; i < b; i++){
+
+		int c = 0;
+		string d;
+		while(inputfile >> d){
+
+			if(d == "."){
+
+				break;
+
+			}
+			else{
+
+			c += UnlimitedSeas[d];
+
+			}
+
+		}
+
+		outputfile << c << endl;
+
+	}
+
 }
+
